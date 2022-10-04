5f82348a9601a6888ffcffc6a9bc8907048f3a7f
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-04 19:16:52 -0300

    Add project files.

---
+++ OOPEnum/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,13 @@
+﻿
+namespace OOPEnum.Entities.Enums
+{
+    //Estamos derivando da classe int
+    enum OrderStatus : int
+    {
+        //Se não atribuirmos valores iniciais ele por padrão inicia em 0
+        PendingPayement,    
+        Processing, 
+        Shipped,    
+        Delivered,  
+    }
+}
