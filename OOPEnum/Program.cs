10cbe9e0c49164e070bb87a48c4fcbd0b1dd2f04
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-04 19:49:18 -0300

    converter Enum to String

--- OOPEnum/Program.cs
+++ OOPEnum/Program.cs
@@ -11,3 +11,12 @@ Order order = new Order()
 };
 
 Console.WriteLine(order);
+
+
+//Convertendo um Enum para String
+string txt = OrderStatus.PendingPayement.ToString();
+Console.WriteLine($"Enum to String: {txt}");
+
+//Convertendo uma String para Enum
+OrderStatus delivered = Enum.Parse<OrderStatus>("Delivered");
+Console.WriteLine($"String to Enum: {delivered}");
