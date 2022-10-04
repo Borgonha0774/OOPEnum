b940955442b20270bf4ac05aae90274764b6931f
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-04 19:36:06 -0300

    instanciado classe order

--- OOPEnum/Entities/Order.cs
+++ OOPEnum/Entities/Order.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OOPEnum.Entities.Enums;
 
 namespace OOPEnum.Entities
 {
@@ -10,5 +6,18 @@ namespace OOPEnum.Entities
     {
         public int Id { get; set; }
         public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return 
+                $"{Id}, "+
+                $"{Moment}, "+
+                $"{Status}";
+                
+
+
+
+        }
     }
 }
--- OOPEnum/Program.cs
+++ OOPEnum/Program.cs
@@ -1,1 +1,13 @@
 ﻿
+using OOPEnum.Entities;
+using OOPEnum.Entities.Enums;
+
+
+Order order = new Order()
+{
+    Id = 1080,
+    Moment = DateTime.Now,
+    Status = OrderStatus.PendingPayement
+};
+
+Console.WriteLine(order);
